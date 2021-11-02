90ed0f20b207688e132ebcee43fa6c3425585b2d
sravan voleti <quic_svoleti@quicinc.com>
2021-11-02 12:20:10 +0530

    Add bt_get_property_bool
    Add bt_get_property_bool with the missing callback
    
    Bug: 199777430
    CRs-Fixed: 3077243
    Change-Id: I2bc5e62929f5c77472000132702bd62d60a29f49

--- osi/src/properties.cc
+++ osi/src/properties.cc
@@ -80,8 +80,8 @@ if(property_callouts)
 
 bool osi_property_get_bool(const char* key, bool default_value) {
 #if defined(OS_GENERIC)
-//if(property_callouts)
-//    return property_callouts->bt_get_property_bool(key, default_value);
+  if(property_callouts)
+    return property_callouts->bt_get_property_bool(key, default_value);
 
   return default_value;
 #else
