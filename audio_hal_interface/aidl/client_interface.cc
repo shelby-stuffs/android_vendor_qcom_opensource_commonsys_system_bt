10a2e144d046f53f4ec7a46b6efa9b73e6a9fc7a
Rajat Soni <quic_rajasoni@quicinc.com>
2022-09-06 10:52:47 +0530

    Added the copyright markings
    Added the copyright markings
    
    Change-Id: I0a38bcc938c25e680aa48bbc903eb4df9cb4634d

--- audio_hal_interface/aidl/client_interface.cc
+++ audio_hal_interface/aidl/client_interface.cc
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/*
+/* Changes from Qualcomm Innovation Center are provided under the following license:
  * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
 
     * Redistribution and use in source and binary forms, with or without
