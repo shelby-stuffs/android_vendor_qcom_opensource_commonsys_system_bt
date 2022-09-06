72e4cc9a8f6d8f47f98cc88dfeb3d4e7e1b7c9e4
zhiwyan <quic_zhiwyan@quicinc.com>
2022-09-06 11:23:44 +0800

    bt-stack: add license comment
    add the following license comment:
    Changes from Qualcomm Innovation Center are
    provided under the following license
    
    CRs-Fixed: 3284405
    
    Change-Id: Id009bde3ab7932f834a7aad95273e00ed42c4de4

--- audio_hal_interface/aidl/le_audio_software.cc
+++ audio_hal_interface/aidl/le_audio_software.cc
@@ -15,6 +15,7 @@
  * limitations under the License.
  */
  /*
+ * Changes from Qualcomm Innovation Center are provided under the following license:
  * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
 
     * Redistribution and use in source and binary forms, with or without
