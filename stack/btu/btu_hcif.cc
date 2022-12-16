0c575df6462667a438d1d9b208b6c4d48273613b
Hui Peng <phui@google.com>
2022-12-16 01:44:15 +0000

    Fix an OOB in btu_ble_rc_param_req_evt
    Bug: 256165737
    Test: manual
    Tag: #security
    Ignore-AOSP-First: security
    
    CRs-Fixed: 3425764
    Merged-In: I0a626bbc1a72c8bc9740d139b54726b188b6f1df
    Change-Id: I0a626bbc1a72c8bc9740d139b54726b188b6f1df
    (cherry picked from commit 5bdefb74e944c86b8665b6627b85818641bac92f)

--- stack/btu/btu_hcif.cc
+++ stack/btu/btu_hcif.cc
@@ -171,7 +171,7 @@ static void btu_ble_proc_ltk_req(uint8_t* p);
 static void btu_hcif_encryption_key_refresh_cmpl_evt(uint8_t* p);
 static void btu_ble_data_length_change_evt(uint8_t* p, uint16_t evt_len);
 #if (BLE_LLT_INCLUDED == TRUE)
-static void btu_ble_rc_param_req_evt(uint8_t* p);
+static void btu_ble_rc_param_req_evt(uint8_t* p, uint8_t len);
 #endif
 #if (BLE_PRIVACY_SPT == TRUE)
 static void btu_ble_proc_enhanced_conn_cmpl(uint8_t* p, uint16_t evt_len);
@@ -403,7 +403,7 @@ void btu_hcif_process_event(UNUSED_ATTR uint8_t controller_id, BT_HDR* p_msg) {
 #endif
 #if (BLE_LLT_INCLUDED == TRUE)
         case HCI_BLE_RC_PARAM_REQ_EVT:
-          btu_ble_rc_param_req_evt(p);
+          btu_ble_rc_param_req_evt(p, ble_evt_len);
           break;
 #endif
         case HCI_BLE_DATA_LENGTH_CHANGE_EVT:
@@ -2117,9 +2117,14 @@ static void btu_ble_data_length_change_evt(uint8_t* p, uint16_t evt_len) {
  * End of BLE Events Handler
  **********************************************/
 #if (BLE_LLT_INCLUDED == TRUE)
-static void btu_ble_rc_param_req_evt(uint8_t* p) {
+static void btu_ble_rc_param_req_evt(uint8_t* p, uint8_t len) {
   uint16_t handle;
   uint16_t int_min, int_max, latency, timeout;
+
+  if (len < 10) {
+    LOG(ERROR) << __func__ << "bogus event packet, too short";
+    return;
+  }
 
   STREAM_TO_UINT16(handle, p);
   STREAM_TO_UINT16(int_min, p);
