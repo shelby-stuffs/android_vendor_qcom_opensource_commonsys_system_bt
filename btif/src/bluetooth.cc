31b5d91a765ca683dbdc329cd8135be7a4a06272
Nitin Shivpure <quic_nshivpur@quicinc.com>
2023-02-12 06:46:10 +0000

    Add default implementation of create_bond_le
    CRs-Fixed: 3427201 
    Change-Id: I508b7fbf71c22a04c7da3b73216739c75ae10727

--- btif/src/bluetooth.cc
+++ btif/src/bluetooth.cc
@@ -323,6 +323,12 @@ static int create_bond(const RawAddress* bd_addr, int transport) {
   return btif_dm_create_bond(bd_addr, transport);
 }
 
+static int create_bond_le(const RawAddress* bd_addr, uint8_t addr_type) {
+  if (!interface_ready()) return BT_STATUS_NOT_READY;
+
+  return BT_STATUS_SUCCESS;
+}
+
 static int create_bond_out_of_band(const RawAddress* bd_addr, int transport,
                                    const bt_oob_data_t* p192_data,
                                    const bt_oob_data_t* p256_data) {
@@ -653,6 +659,7 @@ EXPORT_SYMBOL bt_interface_t bluetoothInterface = {
     start_discovery,
     cancel_discovery,
     create_bond,
+    create_bond_le,
     create_bond_out_of_band,
     remove_bond,
     cancel_bond,
