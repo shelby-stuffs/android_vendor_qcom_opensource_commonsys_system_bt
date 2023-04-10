3a28d97b2ca47538597de170540e2847baa5a889
penghua <quic_penghua@quicinc.com>
2023-04-10 16:42:01 +0800

    Return FAILURE if A2DP active device change is failed
    BT stack returns TRUE in the API of set_active_device if A2DP is disconnected during the process of
    A2DP SHO. It causes MM audio start streaming , but failed. The fix is to return FAILURE if A2DP active
    device change is failed.
    
    Change-Id: I1427f716b4437e8e18f5afaaec1e77ca59529bb7
    CRs-Fixed: 3459418

--- btif/src/btif_av.cc
+++ btif/src/btif_av.cc
@@ -4920,7 +4920,12 @@ static bt_status_t set_active_device(const RawAddress& bd_addr) {
     if (!bd_addr.IsEmpty())
       btif_transfer_context(btif_av_handle_event, BTIF_AV_CHECK_PENDING_PLAY_EVT,
                                     (char *)&bd_addr, sizeof(RawAddress), NULL);
-    return BT_STATUS_SUCCESS;
+    BTIF_TRACE_EVENT("%s: session_wait: %d",__func__, session_wait);
+    if (session_wait) {
+      return BT_STATUS_SUCCESS;
+    } else {
+      return BT_STATUS_FAIL;
+    }
   } else {
     /* Initiate handoff for the device with address in the argument*/
     return btif_transfer_context(btif_av_handle_event,
