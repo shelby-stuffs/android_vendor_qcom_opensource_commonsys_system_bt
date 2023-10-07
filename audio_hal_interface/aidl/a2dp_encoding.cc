40a1d511baab1a25fa78bf241107b2f5e5ad6dcd
Yuan Si <do4suki@gmail.com>
2023-10-08 03:25:41 +0800

    Revert "Support Metadata for aptX Adaptive over A2DP (2/4)."
    This reverts commit 4b9e7df309e81de170d3523f0701138894399509.
    
    Change-Id: I777cba6c0c15cc8316df57a67182a7b67f502461
    Signed-off-by: Yuan Si <do4suki@gmail.com>

--- audio_hal_interface/aidl/a2dp_encoding.cc
+++ audio_hal_interface/aidl/a2dp_encoding.cc
@@ -168,47 +168,20 @@ bool A2dpTransport::GetPresentationPosition(uint64_t* remote_delay_report_ns,
 }
 
 void A2dpTransport::SourceMetadataChanged(
-  const source_metadata_t& source_metadata) {
-
+    const source_metadata_t& source_metadata) {
   auto track_count = source_metadata.track_count;
   auto tracks = source_metadata.tracks;
-
   LOG(INFO) << __func__ << "AIDL: " << track_count << " track(s) received";
-  if (track_count == 0) {
-    LOG(WARNING) << __func__ << ": Invalid number of metadata changed tracks";
-    return;
+  while (track_count) {
+     LOG(INFO) << __func__ << "AIDL: usage=" << tracks->usage
+               << ", content_type=" << tracks->content_type
+               << ", gain=" << tracks->gain;
+    --track_count;
+    ++tracks;
   }
-
-  auto usage = source_metadata.tracks->usage;
-
-  LOG(INFO) << __func__ << ", content_type=" << tracks->content_type
-                        << ", track_count: " << track_count
-                        << ", usage: " << usage;
-
-  btif_ahim_update_src_metadata(source_metadata);
-
 }
 
-void A2dpTransport::SinkMetadataChanged(
-  const sink_metadata_t& sink_metadata) {
-
-  auto track_count = sink_metadata.track_count;
-  auto tracks = sink_metadata.tracks;
-
-  LOG(INFO) << __func__ << "AIDL: " << track_count << " track(s) received";
-  if (track_count == 0) {
-    LOG(WARNING) << __func__ << ": Invalid number of metadata changed tracks";
-    return;
-  }
-
-  auto source = sink_metadata.tracks->source;
-
-  LOG(INFO) << __func__ << ", track_count: " << track_count
-                        << ", source: " << source;
-
-  btif_ahim_update_sink_metadata(sink_metadata);
-
-}
+void A2dpTransport::SinkMetadataChanged(const sink_metadata_t&) {}
 
 tA2DP_CTRL_CMD A2dpTransport::GetPendingCmd() const {
 LOG(ERROR) << ": AIDL Is this function called";
