9128bb5c42f5923cefe9d5750a0f0d642ceaccbe
wentjin <quic_wentjin@quicinc.com>
2023-11-08 17:25:46 +0800

    Added null check for p_rcb
    Root cause: BT crashed caused by null pointer dereference about p_rcb.
    
    Change-Id: Ia85e7df9f6fb9a61a568f29d8cb058f1927fe26f
    CRs-Fixed: 3659352

--- bta/gatt/bta_gattc_act.cc
+++ bta/gatt/bta_gattc_act.cc
@@ -1060,7 +1060,7 @@ void bta_gattc_disc_cmpl(tBTA_GATTC_CLCB* p_clcb,
     bta_gattc_continue(p_clcb);
   }
 
-  if (p_clcb->p_rcb->p_cback && p_clcb->p_srcb) {
+  if (p_clcb->p_rcb && p_clcb->p_rcb->p_cback && p_clcb->p_srcb) {
     tBTA_GATTC bta_gattc;
     bta_gattc.remote_bda = p_clcb->p_srcb->server_bda;
     (*p_clcb->p_rcb->p_cback)(BTA_GATTC_SRVC_DISC_DONE_EVT, &bta_gattc);
